4805cd01ac7f67aa6f6e1ec824d92a7729a84fe9
PHB-1994 <gudqls0991@naver.com>
2025-08-12 15:18:27 +0900

    로그인페이지 html, css 설정, 로그인 버튼 클릭 에러 기능 추가

--- js/login.js
+++ js/login.js
@@ -1,1 +1,19 @@
-$(function () {});
+$(function () {
+  $("#loginBtn2").click(loginFn);
+});
+
+function loginFn(e) {
+  e.preventDefault();
+  const username = $("#username").val().trim();
+  const password = $("#password").val().trim();
+
+  if (username.length == 0) {
+    $("#nameResult").html(`<div class="error">아이디를 입력해주세요.</div>`);
+    $("#username").focus();
+  } else {
+    loginCheck();
+  }
+  if (password.length == 0) {
+    $("#pwResult").html(`<div class="error">비밀번호를 입력해주세요.</div>`);
+  }
+}
