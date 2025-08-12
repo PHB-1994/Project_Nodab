2259faeb66f74c1e82a9cd8ee992155d12d2bfb6
PHB-1994 <gudqls0991@naver.com>
2025-08-12 15:36:44 +0900

    로그인페이지 입력 값이 빈칸일때 메세지 추가

--- js/login.js
+++ js/login.js
@@ -7,13 +7,28 @@ function loginFn(e) {
   const username = $("#username").val().trim();
   const password = $("#password").val().trim();
 
-  if (username.length == 0) {
+  if (username.length == 0 && password.length == 0) {
+    $("#nameResult").html(`<div class="error">아이디를 입력해주세요.</div>`);
+    $("#pwResult").html(`<div class="error">비밀번호를 입력해주세요.</div>`);
+    $("#username").focus();
+  } else if (username.length == 0) {
     $("#nameResult").html(`<div class="error">아이디를 입력해주세요.</div>`);
     $("#username").focus();
+  } else if (password.length == 0) {
+    $("#pwResult").html(`<div class="error">비밀번호를 입력해주세요.</div>`);
+    $("#password").focus();
+    return;
   } else {
     loginCheck();
   }
-  if (password.length == 0) {
-    $("#pwResult").html(`<div class="error">비밀번호를 입력해주세요.</div>`);
-  }
 }
+
+function loginCheck(e) {
+  e.preventDefault();
+}
+
+//   if (username.length > 0) {
+//     $("#nameResult").removeClass("error").text("");
+//   } else if (password.length > 0) {
+//     $("#password").removeClass("error").text("");
+//   }
